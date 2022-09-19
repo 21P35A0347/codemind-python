a4647959769fa88890722926288dd87ce175f907
DOKKADI EMMANUEL <106660939+21P35A0347@users.noreply.github.com>
2022-09-19 16:01:21 +0530

    def is_prime(n):     fc=0     for i in range(1,n//2+1):         if n%i==0:             fc+=1     if fc==1:         return True     else:         return False a=input() l=list(map(int,input().split())) k=int(input()) c=0 for i in l:     if is_prime(i)==True and i<=k:         c+=1 print(c)

---
+++ primes_less_than_k.py
@@ -0,0 +1,18 @@
+def is_prime(n):
+    fc=0
+    for i in range(1,n//2+1):
+        if n%i==0:
+            fc+=1
+    if fc==1:
+        return True
+    else:
+        return False
+a=input()
+l=list(map(int,input().split()))
+k=int(input())
+c=0
+for i in l:
+    if is_prime(i)==True and i<=k:
+        c+=1
+print(c)
+        
